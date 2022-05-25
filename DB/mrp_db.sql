4ed06fb3083b227ef27e4621d7ddbca148fca936
Edwin-AVP <edwinvalencia97@gmail.com>
2022-05-24 22:12:55 -0500

    Actualizacion BD

--- DB/mrp_db.sql
+++ DB/mrp_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-05-2022 a las 23:57:33
+-- Tiempo de generación: 25-05-2022 a las 05:11:20
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -30,12 +30,22 @@ SET time_zone = "+00:00";
 CREATE TABLE `cliente` (
   `ID_CLIENTE` int(11) NOT NULL,
   `nombreCliente` varchar(100) NOT NULL,
-  `IdentificacionCliente` int(11) NOT NULL,
+  `IdentificacionCliente` bigint(15) NOT NULL,
   `tipoIdentificacion` varchar(50) NOT NULL,
   `telefono` varchar(11) NOT NULL,
   `direccion` varchar(50) NOT NULL,
   `correo` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`ID_CLIENTE`, `nombreCliente`, `IdentificacionCliente`, `tipoIdentificacion`, `telefono`, `direccion`, `correo`) VALUES
+(1, 'John jose bartolomedo', 9001856721, 'NIT', '7584269', 'Calle 34 # 5-77', 'johnjose21@gmail.com'),
+(2, 'Variedades chucho', 9005836581, 'NIT', '3224596885', 'Carrera 94 # 6-51', 'variedadeschucho@gmail.com'),
+(3, 'John jose bartolomedo', 9001856721, 'NIT', '7584269', 'Calle 34 # 5-77', 'johnjose21@gmail.com'),
+(4, 'Variedades chucho', 9005836581, 'NIT', '3224596885', 'Carrera 94 # 6-51', 'variedadeschucho@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -53,6 +63,17 @@ CREATE TABLE `material` (
   `cantidadMaterialInventario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `material`
+--
+
+INSERT INTO `material` (`ID_MATERIAL`, `nombreMaterial`, `codigoMaterial`, `unidadMedidaMaterial`, `valorMaterial`, `tipoMaterial`, `cantidadMaterialInventario`) VALUES
+(1, 'A', 'MT010', 'Kg', 1000, 'Materia Prima', 900),
+(2, 'B', 'MT011', 'Kg', 1500, 'Materia Prima', 1500),
+(3, 'C', 'MT012', 'Kg', 1200, 'Materia Prima', 800),
+(4, 'D', 'MT013', 'Kg', 1100, 'Materia Prima', 1400),
+(5, 'E', 'MT014', 'Kg', 500, 'Materia Prima', 450);
+
 -- --------------------------------------------------------
 
 --
@@ -61,11 +82,20 @@ CREATE TABLE `material` (
 
 CREATE TABLE `orden` (
   `ID_ORDEN` int(11) NOT NULL,
-  `numeroOrden` int(11) NOT NULL,
+  `numeroOrden` varchar(20) NOT NULL,
   `fecha` date NOT NULL,
   `estado` int(11) NOT NULL,
   `FK_ID_CLIENTE` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `orden`
+--
+
+INSERT INTO `orden` (`ID_ORDEN`, `numeroOrden`, `fecha`, `estado`, `FK_ID_CLIENTE`) VALUES
+(3, 'TP020', '2022-05-22', 1, 1),
+(4, 'TP021', '2022-05-22', 1, 2),
+(5, 'TP022', '2022-05-22', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -80,6 +110,17 @@ CREATE TABLE `ordenproducto` (
   `FK_ID_PRODUCTO` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `ordenproducto`
+--
+
+INSERT INTO `ordenproducto` (`ID_ORDEN_PRODUCTO`, `cantidadProductoSolicitado`, `FK_ID_ORDEN`, `FK_ID_PRODUCTO`) VALUES
+(1, 20, 3, 3),
+(2, 10, 3, 4),
+(3, 5, 4, 4),
+(4, 6, 5, 3),
+(5, 20, 5, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -93,6 +134,14 @@ CREATE TABLE `producto` (
   `valorUnidad` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`ID_PRODUCTO`, `nombreProducto`, `codigoProducto`, `valorUnidad`) VALUES
+(3, 'Shampoo Limón 250ml', 'PR020', 13770),
+(4, 'Shampoo Limón 500ml', 'PR021', 27540);
+
 -- --------------------------------------------------------
 
 --
@@ -101,10 +150,22 @@ CREATE TABLE `producto` (
 
 CREATE TABLE `productomaterial` (
   `ID_PRODUCTO_MATERIAL` int(11) NOT NULL,
-  `cantidadMaterialProducto` int(11) NOT NULL,
+  `cantidadMaterialProducto` float NOT NULL,
   `FK_ID_PROCUCTO` int(11) NOT NULL,
   `FK_ID_MATERIAL` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productomaterial`
+--
+
+INSERT INTO `productomaterial` (`ID_PRODUCTO_MATERIAL`, `cantidadMaterialProducto`, `FK_ID_PROCUCTO`, `FK_ID_MATERIAL`) VALUES
+(3, 5, 3, 1),
+(4, 10, 3, 2),
+(5, 8, 3, 3),
+(6, 6, 3, 4),
+(7, 9, 4, 5),
+(8, 12, 4, 3);
 
 -- --------------------------------------------------------
 
@@ -145,7 +206,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`ID_USUARIO`, `username`, `password`, `FK_ID_ROL`, `nombre`, `cedula`) VALUES
-(1, 'edwin', '1234', 1, 'Edwin Valencia', '1144093816'),
+(1, 'admin', '1234', 1, 'Edwin Valencia', '1144093816'),
 (2, 'colab', '9716', 2, 'Fernando Buenavida', '1144096816');
 
 --
@@ -214,37 +275,37 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `material`
 --
 ALTER TABLE `material`
-  MODIFY `ID_MATERIAL` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_MATERIAL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `orden`
 --
 ALTER TABLE `orden`
-  MODIFY `ID_ORDEN` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_ORDEN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `ordenproducto`
 --
 ALTER TABLE `ordenproducto`
-  MODIFY `ID_ORDEN_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_ORDEN_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `ID_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `productomaterial`
 --
 ALTER TABLE `productomaterial`
-  MODIFY `ID_PRODUCTO_MATERIAL` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_PRODUCTO_MATERIAL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -266,14 +327,14 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `orden`
 --
 ALTER TABLE `orden`
-  ADD CONSTRAINT `cliente_FK` FOREIGN KEY (`FK_ID_CLIENTE`) REFERENCES `cliente` (`ID_CLIENTE`),
-  ADD CONSTRAINT `orden_ibfk_1` FOREIGN KEY (`ID_ORDEN`) REFERENCES `ordenproducto` (`FK_ID_ORDEN`);
+  ADD CONSTRAINT `cliente_FK` FOREIGN KEY (`FK_ID_CLIENTE`) REFERENCES `cliente` (`ID_CLIENTE`);
 
 --
--- Filtros para la tabla `producto`
+-- Filtros para la tabla `ordenproducto`
 --
-ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`ID_PRODUCTO`) REFERENCES `ordenproducto` (`FK_ID_PRODUCTO`);
+ALTER TABLE `ordenproducto`
+  ADD CONSTRAINT `ordenproducto_ibfk_1` FOREIGN KEY (`FK_ID_PRODUCTO`) REFERENCES `producto` (`ID_PRODUCTO`),
+  ADD CONSTRAINT `ordenproducto_ibfk_2` FOREIGN KEY (`FK_ID_ORDEN`) REFERENCES `orden` (`ID_ORDEN`);
 
 --
 -- Filtros para la tabla `productomaterial`
